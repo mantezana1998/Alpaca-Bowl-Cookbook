405fddf6852acd1010bc787002b2d7f44fec8f26
mantezana <maximo00antezana@gmail.com>
2021-09-30 09:44:26 -0700

    hello

--- models/user.js
+++ models/user.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 // Create your User Model
 
-const reviewSchema = new mongoose.Schema ({
-    review: String,
-    rating: Number //WHAT THE FUCK IS GOING ON 
-});
+// const reviewSchema = new mongoose.Schema ({
+//     review: String,
+//     rating: Number
+// });
 
-module.exports = mongoose.model('Review', reviewSchema);
+// module.exports = mongoose.model('Review', reviewSchema);
 
