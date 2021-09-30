023b39855d7a67f73e7cc5073fcce51a9c2109e7
mantezana <maximo00antezana@gmail.com>
2021-09-30 09:55:22 -0700

    fixed merge

--- config/database.js
+++ config/database.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 // replace your database connection string here
-<<<<<<< HEAD
 mongoose.connect(process.env.DATABASE_URL ,{
-=======
-mongoose.connect('mongodb://localhost/test' ,{
->>>>>>> 092a57601ba5186e593d07fdd554c3283a87343f
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 // Create your User Model
 
 // const reviewSchema = new mongoose.Schema ({
@@ -10,6 +9,4 @@ const mongoose = require('mongoose');
 
 // module.exports = mongoose.model('Review', reviewSchema);
 
-=======
 // Create your User Model
->>>>>>> 092a57601ba5186e593d07fdd554c3283a87343f
