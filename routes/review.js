5d2899da064347f924409aa5b078769f96d72a19
mantezana <maximo00antezana@gmail.com>
2021-10-06 16:32:55 -0700

    loggedin function and logout button completed

--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,14 @@
 var router = require('express').Router()
 var reviewsCtrl = require('../controllers/reviews');
 
-router.post('/cookbook/:id/reviews', reviewsCtrl.create);
-router.delete('/reviews/:id', reviewsCtrl.delete);
-router.put('/reviews/:id', reviewsCtrl.update);
+router.post('/cookbook/:id/reviews', isLoggedIn, reviewsCtrl.create);
+router.delete('/reviews/:id', isLoggedIn, reviewsCtrl.delete);
+router.put('/reviews/:id', isLoggedIn, reviewsCtrl.update);
+
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+}
 
 module.exports = router;
 
