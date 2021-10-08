0c3e9e8000f72a59c455b0f0b1bd2455a043be42
mantezana <maximo00antezana@gmail.com>
2021-10-07 19:00:20 -0700

    draft

--- models/recipe.js
+++ models/recipe.js
@@ -11,7 +11,7 @@ const recipeSchema = new mongoose.Schema ({
     recipe: String,
     slug: String,
     ingredient: Array,
-    instruction: Array,
+    instruction: String,
     description: String,
     picture: String,
     review: [reviewSchema],
