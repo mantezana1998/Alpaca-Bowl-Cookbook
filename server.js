a09008f246618a049608fd28d20ff87dda0794ca
mantezana <maximo00antezana@gmail.com>
2021-10-07 19:32:56 -0700

    wtttttt

--- server.js
+++ server.js
@@ -2,20 +2,16 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-// session middleware
 var session = require('express-session');
 var passport = require('passport');
 var methodOverride = require('method-override');
 
-// load the env vars
 require('dotenv').config();
 
-// create the Express app
 var app = express();
 
-// connect to the MongoDB with mongoose
+
 require('./config/database');
-// configure Passport
 require('./config/passport');
 
 
@@ -23,7 +19,6 @@ var indexRoutes = require('./routes/index');
 var cookbookRoutes = require('./routes/cookbook');
 var reviewRoutes = require('./routes/review');
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -33,7 +28,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// mount the session middleware
 app.use(session({
   secret: 'SEI Rocks!',
   resave: false,
@@ -44,20 +38,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-// Add this middleware BELOW passport middleware
 app.use(function (req, res, next) {
-  res.locals.user = req.user; // assinging a property to res.locals, makes that said property (user) availiable in every
-  // single ejs view
+  res.locals.user = req.user; 
   next();
 });
 
-// mount all routes with appropriate base paths
 app.use('/', indexRoutes);
 app.use('/cookbook', cookbookRoutes);
 app.use('/', reviewRoutes);
 
 
-// invalid request, send 404 page
 app.use(function(req, res) {
   res.status(404).send('Cant find that!');
 });
